626b9c3841dca5f9506e8119f83688dbbd8d8f09
cxmarcelo <marcelo.c.b22@gmail.com>
2022-09-03 16:02:44 -0300

    CRUD para restaurante

---
+++ ifood/cadastro/src/main/java/com/github/marcelo/ifood/entities/Localizacao.java
@@ -0,0 +1,20 @@
+package com.github.marcelo.ifood.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="localizacao")
+public class Localizacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	public Double latitude;
+	
+	public Double longitude;
+}
